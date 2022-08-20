017f2a8730458b574d9b123d0409f3f81e496702
amardip-tayade <amardiptayade@gmail.com>
2022-08-20 18:55:58 +0530

    two way binding

--- basic/src/components/NewExpense/NewExpense.js
+++ basic/src/components/NewExpense/NewExpense.js
@@ -1,13 +1,12 @@
-import React from "react";
-import "./NewExpense.css";
-import ExpenseForm from "./ExpenseForm";
+import React from 'react';
+
+import ExpenseForm from './ExpenseForm';
+import './NewExpense.css';
 
 const NewExpense = () => {
   return (
-    <div className="new-expense">
-      <form>
-        <ExpenseForm />
-      </form>
+    <div className='new-expense'>
+      <ExpenseForm />
     </div>
   );
 };
